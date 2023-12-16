73086d30858d4afd0930caa301bd76f307c5ba3b
Jim Campbell <jimcampbell355@gmail.com>
2023-12-15 23:55:41 -0500

    Added Controller

---
+++ NFL.OData.Service/Controllers/NFLPlayController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query; // EnableQuery
+using Microsoft.AspNetCore.OData.Routing.Controllers; // ODataController
+using NFL.SqlServer.DataContext;
+
+namespace NFL.OData.Service.Controllers;
+
+public class NFLPlayController : ODataController
+{
+    protected readonly NFLDataContext _context;
+    public NFLPlayController(NFLDataContext context)
+    {
+        _context = context;
+    }
+    [EnableQuery(PageSize = 50)]
+    public IActionResult Get()
+    {
+        return Ok(_context.NFLPlays);
+    }
+}
