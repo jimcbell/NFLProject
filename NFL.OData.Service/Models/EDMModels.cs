329a64cb3b8452d8091ac7a081ae2adc9fa5f9b7
Jim Campbell <jimcampbell355@gmail.com>
2023-12-15 23:55:48 -0500

    Added model

---
+++ NFL.OData.Service/Models/EDMModels.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.OData.Edm; // ODataConventionModelBuilder
+using Microsoft.OData.ModelBuilder;
+using NFL.SqlServer.DataContext.Entities; // IEdmModel
+
+namespace NFL.OData.Service.Models;
+
+public static class EDMModels
+{
+    public static IEdmModel GetEdmModelForNFLPlay()
+    {
+        var builder = new ODataConventionModelBuilder();
+        builder.EntitySet<NFLPlay>("NFLPlay");
+        return builder.GetEdmModel();
+    }
+}
