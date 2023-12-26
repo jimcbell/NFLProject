fe46ec27295add8aea223a3fa7a9a0191e97c7ef
Jim Campbell <jimcampbell355@gmail.com>
2023-12-26 17:40:57 -0500

    Uncommented so it is easier to copy paste.

--- Nfl.SqlServer.DataContext/Queries/pass-type-setup.sql
+++ Nfl.SqlServer.DataContext/Queries/pass-type-setup.sql
@@ -1,10 +1,10 @@
 -- Insert the different pass types into the pass type table.
---Insert into dbo.NflPassType select distinct PassType from dbo.NflPlay
+Insert into dbo.NflPassType select distinct PassType from dbo.NflPlay
 
 -- Update the database with references to the new pass type table.
---Update NflPlay
---SET NflPlay.NflPassTypeId = NflPassType.Id
---From NflPlay inner join NflPassType on NflPlay.PassType = NflPassType.[Description]
+Update NflPlay
+SET NflPlay.NflPassTypeId = NflPassType.Id
+From NflPlay inner join NflPassType on NflPlay.PassType = NflPassType.[Description]
 
 -- There is an empty pass type and play type for when records don't have these, find the id of this in the table.
 DECLARE @EmptyPassTypeId as Int
@@ -13,10 +13,10 @@ SET @EmptyPassTypeId = (select Id from dbo.NflPassType Where [Description] = '')
 --Select * from dbo.NflPassType Where Id = @EmptyPassTypeId
 
 -- Make these references null to the empty pass type as we are going to remove it from the table.
---Update dbo.NflPlay 
---Set NflPassTypeId = NULL
---Where NflPassTypeId = @EmptyPassTypeId
+Update dbo.NflPlay 
+Set NflPassTypeId = NULL
+Where NflPassTypeId = @EmptyPassTypeId
 
 -- Delete the empty pass type from the pass type table
---Delete from dbo.NflPassType Where Id = @EmptyPassTypeId
+Delete from dbo.NflPassType Where Id = @EmptyPassTypeId
 
